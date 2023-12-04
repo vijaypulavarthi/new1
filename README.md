593c38f09edb716e0b72b9db92ff48b331993fe2
vijay pulavarthi <spulavar@iu.edu>
2023-12-04 17:23:25 -0500

    dhd

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# new1
+# Purpose:
+
+Human trafficking is a significant issue in this country, and the topic of sex trafficking is of great importance. In this exercise, I will be examining a human trafficking dataset compiled from various local, state, city, county, and federal agencies reporting human trafficking cases in the United States from 2013-2021. 
+
+By analyzing this data, we can better understand the prevalence of human trafficking in different regions of the country and potentially identify connections to specific groups or areas. To analyze the dataset, I will use Python and several libraries such as matplotlib, seaborn, numpy, and pandas. After cleaning and preprocessing the data, I will use visualizations and statistical techniques to identify trends and patterns in the data, such as using bar charts, scatter plots, and line graphs to visualize the number of human trafficking cases over time or by location. By using these tools, we can gain a deeper understanding of the issue of human trafficking and work towards finding solutions to prevent future tragedies.
+
+## Data Description:
+
+Below are explanations of what is found in each column of the Human Trafficking dataset.
+
+DATA_YEAR – The year in which the incident occurred.
+
+ORI – ORIGINATING AGENCY IDENTIFIER (ORI) - This identifies the agency in which the offense occurred.
+
+PUB_AGENCY_NAME – Agency name as it appears in FBI UCR Publications.
+
+PUB_AGENCY_UNIT – The specific unit name for which a Publication Agency report UCR data as.
+
+AGENCY_TYPE_NAME – Type of agency that reports UCR data (city/county/federal agency, etc).
+
+STATE_ABBR – This is the state abbreviation.
+
+STATE_NAME – Full name of the state.
+
+DIVISION_NAME – The geographic division in which the agency is located.
+
+COUNTY_NAME – The name of the county within the state.
+
+REGION_NAME – Geographic region in which the agency is located.
+
+POPULATION_GROUP_CODE – Group 0 is possessions; 1-7 are cities; 8-9 are counties.
+
+POPULATION_GROUP_DESC – The name of the population groups.
+
+OFFENSE_SUBCAT_ID – A numeric code assigned to the Offense_Subcat_Name.
+
+ACTUAL_COUNT – Total number of Human Trafficking offenses reported to the UCR Program.
+
+UNFOUNDED_COUNT – Total number of false or baseless complaints reported to the UCR Program from law enforcement agencies.
+
+CLEARED_COUNT – Total number of Human Trafficking offenses that were cleared or closed.
+
+JUVENILE_CLEARED_COUNT – Total number of Human Trafficking offenses that involved a juvenile offender that were cleared or closed.
+
+OFFENSE_SUBCAT_NAME
+
+Commercial Sex Acts – Human trafficking commercial sex acts is defined as inducing a person by force, fraud, or coercion to participate in commercial sex acts, or in which the person induced to perform such act(s) has not attained 18 years of age.
+
+Involuntary Servitude –Human trafficking involuntary servitude is defined as the obtaining of a person(s) through recruitment, harboring, transportation, or provision, and subjecting such persons by force, fraud, or coercion into involuntary servitude, peonage, debt bondage, or slavery (not to include commercial sex acts).
+
+##  Algorithm Description:
+
+Data Loading and Display: Load the dataset using Pandas, display the initial rows, summary statistics, and information about numeric and categorical columns.
+Percentage Analysis: Calculate and display the percentage of occurrences for each unique value in each column and the percentage of null values in each column.
+Top Unsolved Cases: Determine and display the top 10 percentages of unsolved cases.
+Specific Analysis:
+Analyze reporting agency percentages.
+Count data entries per year to observe trends.
+Analyze specific cases related to tribal agencies, Florida, Seminole County, juvenile cases cleared, and cases from Nevada and Florida.
+Visualization:
+Generate various visualizations using Seaborn and Matplotlib to depict reported cases by state, year, offense subcategory, etc.
+Statistics and Rankings:
+Calculate statistics like the breakdown of reported offenses, ranking top states based on occurrences, and displaying the maximum occurrences by county, state, population group, and region.
+
+## Tools Used:
+Streamlit: Used for building the web application, creating an interactive interface, and displaying data.
+Pandas: For data manipulation, loading, cleaning, and analyzing the dataset.
+Seaborn & Matplotlib: Used for data visualization, creating various plots (bar, line, scatter) to represent trends, distributions, and comparisons visually.
--- main.py
+++ main.py
@@ -43,10 +43,13 @@ st.subheader('Percentage of Null  Values')
 for col in df.columns:
     if df[col].isna().sum():
         st.write(f"{col}: {round(df[col].isna().sum() / len(df), 3) * 100}%")
+st.write('Unfounded Count has 1,969 blank values accounting for 63.6% of the dataset, and Public Agency Unit has 2,867 null values for 92.5% of the dataset. There are three apiece for Population Group Code and Population Group Desc. accounting for just .1% of the dataset (minimal). It seems that Unfounded_Count null values likely can be interpreted as zero; that who did data entry just left that blank if there were none.')      
 
  # Title and description
 st.title('Top 10 Percentages  of Unsolved Cases')
 st.write('This section displays the top 10 percentages of unsolved cases from the FBI dataset.')
+st.write('''Just over half of the cases in this entire 8-year dataset remain unsolved? Is this possible? This would warrant a deeper examination.
+Next I'll look at which agencies are reporting, or investigating, trafficking-related crimes.''')
 
 # Load data
 df = load_data()
@@ -68,12 +71,13 @@ st.write(yearly_counts)
 # Analysis description
 st.write("There has been increasing participation since the inception of data collection. It's possible that data was only partially reported in 2013.")
 
-# Analyzing data about tribal agencies reporting criminal trafficking
+st.write('Next I would like to look at just tribal agencies who filed criminal trafficking reports.') 
 tribal_agencies = df.query("AGENCY_TYPE_NAME.str.contains('Tribal')")
 st.write('Tribal Agencies Reporting Criminal Trafficking:')
 st.write(tribal_agencies)
 
 # Analyzing Florida cases
+st.write('Note that county name is not specified on tribal lands, nor in federal cases. Next Ill look at Florida cases.')
 florida_cases = df.query("STATE_ABBR.str.contains('FL')")
 st.write('Florida Cases:')
 st.write(florida_cases)
@@ -82,6 +86,8 @@ st.write(florida_cases)
 st.subheader('Seminole County, Florida Data:')
 seminole_data = df.query("COUNTY_NAME.str.contains('SEMINOLE')")
 st.write(seminole_data)
+st.write('''Seminole County, Florida has reported four of the eight years. They are reporting a 100% success rate for solving cases in every year reported.
+Next under the magnifying glass is departments reporting juvenile cases that have been solved:''')
 
 # Juvenile cases cleared/solved by departments
 st.subheader('Juvenile Cases Cleared:')
@@ -89,12 +95,19 @@ juvenile_cleared = df.query("JUVENILE_CLEARED_COUNT > 0")
 st.write(juvenile_cleared)
 st.write(f"Total rows: {len(juvenile_cleared)}")
 
+st.write('''Listed above are jurisdictions with juvenile cases that were cleared, or solved. There are 103 rows of information submitted from different agencies. Here we see that several counties can be listed in one cell, making this an unreliable cell to classify.
+Based on graphs found on the next page, I'm going to focus on Nevada state information for a moment, along with  state of Florida. I'll sort data based on number of cases in Nevada and Florida, returning top 15 number of cases in a given year.''')
+
 # Focus on Nevada and Florida data
 st.subheader('Top 15 Cases - Nevada & Florida:')
 state_data = df.loc[(df.STATE_NAME.isin(['Nevada', 'Florida'])) & (df.OFFENSE_SUBCAT_NAME == 'Commercial Sex Acts'), ['DATA_YEAR', 'STATE_NAME', 'ACTUAL_COUNT']].sort_values(by='ACTUAL_COUNT', ascending=False).head(15)
 st.write(state_data)
 
+st.write('Nevada has held the top 6 spots statewide before Florida makes its first appearance in this list in 2020 with 5,888 cases.')
+
 # Sort reported cases and plot
+st.title('Graphing Relationships')
+st.write('Sorting number of reported cases in descending order and assigned to a new variable name, then using matplotlib to design a graph showing cases reported by state.')
 df_sorted = df.sort_values('ACTUAL_COUNT', ascending=False)
 fig1, ax1 = plt.subplots(figsize=(20, 8))
 plt.style.use("fivethirtyeight")
@@ -111,10 +124,13 @@ fig, ax = plt.subplots()
 ax.plot(df.groupby('DATA_YEAR').sum().ACTUAL_COUNT.index, df.groupby('DATA_YEAR').sum().ACTUAL_COUNT.values)
 ax.set_title("Aggregated Reported Cases by Year")
 st.pyplot(fig)
+st.write('As shown below, the average actual count of human trafficking offenses have increased over the years, or reporting of them has increased. Its important to take into account that 2013 may have only had partial reporting and should not be included in any summary statistics.')
 
 
 # Plotting a line plot of the number of solved cases each year
+
 st.subheader('Number of Solved Cases Each Year')
+st.write('Creating a line plot of the number of solved cases each year, showing a general trend down.')
 fig3, ax3 = plt.subplots()
 sns.lineplot(x="DATA_YEAR", y="CLEARED_COUNT", data=df, ax=ax3)
 st.pyplot(fig3)
@@ -126,12 +142,16 @@ max_REGION_NAME = df['REGION_NAME'].value_counts().index[0]
 st.write('Region with highest occurrence:', max_REGION_NAME)
 st.pyplot()
 
+st.write('Above, note the number of reported cases each year by region - with the west showing the highest data points recorded but a larger number of cases, or data points overall in the South.')
+
 
 # Plot reported cases by state
 plt.figure(figsize=(8, 6))
 sns.catplot(x="ACTUAL_COUNT", y="STATE_NAME", data=df, height=10).set(title="Reported Cases of Trafficking by State")
 st.write("Reports of trafficking for each state")
 st.pyplot()
+st.write('''In the graph above, we see reports of trafficking for each state. Each dot is a reporting agency and Nevada far and away has greater numbers of cases reported by some six departments. Texas is second, with Kentucky showing a high case count for one of its reporting agencies.
+Actual count is actually a binary either/or category of crimes that are sex acts or are not sex acts. What's the breakdown of each?''')
 
 # Breakdown of reported offenses involving sex acts or not
 fig6, ax6 = plt.subplots(figsize=(10, 10))
@@ -152,6 +172,7 @@ st.subheader('Breakdown of Each Crime Reported by Year')
 
 fig7 = sns.lmplot(x="DATA_YEAR", y="ACTUAL_COUNT", hue="OFFENSE_SUBCAT_NAME", data=df, height=6, aspect=2)
 st.pyplot(fig7)
+st.write('Sex trafficking-related crimes are in blue, and we can see more instances of them being reported at higher aggregate numbers. Involuntary servitude is in red, and is reported in low quantities, with a few outlying instances.')
 
 
 
@@ -172,7 +193,10 @@ st.write('Metropolitan size with highest occurrence:', max_POPULATION_GROUP_DESC
 st.write('Region with highest occurrence:', max_REGION_NAME)
 
 st.write("""Based on the analysis, I have observed that Texas had the highest overall occurrences of human trafficking, with cases aroung 186548. Hennepin County, Minnesota had the highest number of cases among for all counties in the United States. The western region is showing the highest individual data points recorded but a larger overall number of cases regionally in the South.
+         
 Data reporting has steadily increased each year since reporting began in 2013. Its important to note that reporting may be incomplete or partial in 2013 and should be taken into account when comparing to other years in the dataset. In this 8-year dataset, there were a total of 13056 reported cases of human trafficking, 3264 of which were deemed unfounded and 6976 cases were solved, or cleared.
+         
 Over 64% of city police departments are reporting human trafficking cases. County law enforcement departments make up 25% of reporting, with state police bringing in 8% of reported cases and Tribal police departments reporting just .3% of cases overall. While Seminole County, Florida has reported four of eight years, they are reporting a 100% success rate for solving cases in every year reported.
 That’s not the case for other departments.
+         
 Just like getting to know people, the more we interact with something, the more we get to know it and are sometimes surprised by what we learn. For purposes of practicing curiosity, if I were to continue studying this data, I would immediately dig deeper into the fact that some 54.35% of the human trafficking cases reported to the FBI are going unsolved each year. That seems a grave number to admit and warrants more investigation. Assuming one person equals one case, that’s 6080 people who continue to be victimized, subjected into slavery, sexual or otherwise in this country, since 2013. This is a huge concern and has eclipsed my initial questions.""")
